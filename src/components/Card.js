947ee19cffba05c8c09d1373d1a12c109f23a601
Tnaike <timmynaike@gmail.com>
2022-12-26 12:01:06 +0100

    Card component implementation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,34 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import { Heading, HStack, Image, Text, VStack, Box } from '@chakra-ui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 
 const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
   return (
-      <div>
-        <VStack>
-          <Image src={imageSrc} alt="Project Image"></Image>
-          <Heading as="h3">{title}</Heading>
-          <Text noOfLines={2} fontSize='lg'>{description}</Text>
+    <div>
+      <VStack bg='white' borderRadius='12'>
+        <Image
+          src={imageSrc}
+          alt='Project Image'
+          borderRadius='12'
+          boxSize='100%'
+        ></Image>
+        <Box p={15} w='100%' color='black'>
+          <Heading as='h3' size='md' mb={4}>
+            {title}
+          </Heading>
+          <Text noOfLines={2} fontSize='md' mb={4} color='gray.500'>
+            {description}
+          </Text>
           <HStack>
-            <Text fontSize='md'>See more</Text>
-            <FontAwesomeIcon icon={faArrowRight} size="1x" />
+            <Text fontSize='md' fontWeight='semibold'>
+              See more
+            </Text>
+            <FontAwesomeIcon icon={faArrowRight} size='1x' />
           </HStack>
-        </VStack>
-      </div>
+        </Box>
+      </VStack>
+    </div>
   );
 };
 
